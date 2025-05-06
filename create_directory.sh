9493ed681772a725f01df486bbfb1415adf2bf6f
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-06 02:19:17 +0200

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-# Ce script crée un répertoire nommé "test_directory" s'il n'existe pas déjà
+# Ce script crée un répertoire nommé test_directory s'il n'existe pas déjà
 
 if [ -d "test_directory" ]; then
   echo "Le répertoire 'test_directory' existe déjà."
 else
   mkdir test_directory
-  echo "Répertoire 'test_directory' créé avec succès."
+  echo "Le répertoire 'test_directory' a été créé."
 fi
